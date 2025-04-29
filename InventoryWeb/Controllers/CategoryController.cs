20e09a3ee897141490445b1bb2812e60e14ba911
usama9293 <usamaarshad9293@gmail.com>
2025-04-29 12:02:03 +0500

    Add CRUD functionality for categories and UI enhancements
    Updated `CategoryController.cs` to include actions for creating, editing, and deleting categories with model validation. Corrected a typo in `Index.cshtml` and improved the layout with new management cards. Created `Delete.cshtml` for category deletion confirmation and `Edit.cshtml` for editing category details, including validation scripts for client-side validation.

--- InventoryWeb/Controllers/CategoryController.cs
+++ InventoryWeb/Controllers/CategoryController.cs
@@ -21,6 +21,9 @@ namespace InventoryWeb.Controllers
         {
             return View();
         }
+
+
+
         [HttpPost]
         public IActionResult Create(Category obj)
         {
@@ -33,5 +36,77 @@ namespace InventoryWeb.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (_db.Categories.Any(c => c.Name == obj.Name && c.Id != obj.Id))
+            {
+                ModelState.AddModelError("Name", "Another category with this name already exists.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET: Category/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST: Category/Delete
+        [HttpPost, ActionName("Delete")]
+       
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
+        }
+
     }
+
 }
